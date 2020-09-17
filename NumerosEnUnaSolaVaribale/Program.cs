587a76e8c8464f1e45fcc88335d7226a7a97346e
BlackMProgrammer <37740584+BlackMProgrammer@users.noreply.github.com>
2020-09-16 20:29:47 -0400

    Add project files.

---
+++ NumerosEnUnaSolaVaribale/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace NumerosEnUnaSolaVaribale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList alInt = new ArrayList();
+            bool truf;
+            do
+            {
+                Console.WriteLine("Digite el numero que desee agregar.");
+
+                alInt.Add(Console.ReadLine());
+
+                Console.WriteLine("Desea agregar mas numeros? S/N");
+                string son = Console.ReadLine().ToLower();
+
+                if (son == "s")
+                {
+                    truf = true;
+                }else{
+                    truf = false;
+                }
+            } while (truf == true);
+
+            foreach (string num in alInt)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
